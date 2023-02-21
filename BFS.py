1beba81fb20bc83c9bfd126a49dbf246f0cd3fc4
br3aded <kelseyRholdgate@icloud.com>
2023-02-21 19:31:11 +0000

    Added comments

--- BFS.py
+++ BFS.py
@@ -1,52 +1,42 @@
 import time
 
 def breadth_first_search(file_name):
-    st =  time.time()
-    f = open(file_name+".txt", "r")
-    data = f.readlines()
+    st =  time.time() #stores the time when the search starts
+    f = open(file_name+".txt", "r") #opens maze file 
+    data = f.readlines() #splits the maze file lines into individual elements
     maze = []
-    for x in data:
+    for x in data: #splits each line into individual elements in the maze (# , -)
         if x.split != []:
             maze.append(x.split())
-    start_position = (maze[0].index('-'),0)
-    goal = (maze[len(maze)-1].index('-'),len(maze)-1)
-    breadth_first_search_loop(maze,[[start_position]],[[goal]])
-    et = time.time()
-    print(file_name + " took " + str(et-st) + " seconds to execute" )
+    start_position = (maze[0].index('-'),0) # finds the start position of the maze
+    goal = (maze[len(maze)-1].index('-'),len(maze)-1) # finds the end position of the maze
+    breadth_first_search_loop(maze,[[start_position]],[[goal]]) # runs the breadth first search by passing the maze array , a foward search queue with the start position already in and a backward search queue with the end position already in
+    et = time.time()  #stores the end time of the search
+    print(file_name + " took " + str(et-st) + " seconds to execute" ) #calculates total time for search to complete and outputs
 
 
 def breadth_first_search_loop(maze,queue_foward,queue_backward):
-    nodes = 1
-    foward_explored = []
-    backward_explored = []
-    while True:
-        path_foward = queue_foward.pop(0)
-        node_foward = path_foward[-1]      
-        if node_foward in backward_explored:
-            for i in range(len(backward_explored)):
-                if node_foward in queue_backward[-i]:
-                    reverse_list = (queue_backward[-i])[::-1]
-                    if len(path_foward) > len(reverse_list):
-                        for i in path_foward:
-                            if i in reverse_list:
-                                path_foward.remove(i)
-                    else:
-                        for i in reverse_list:
-                            if i in path_foward:
-                               reverse_list.remove(i)
-                    path = path_foward + reverse_list
-                    for x in path:
+    foward_explored = [] # creates variable foward_explored as an empty array (used to keep track of all nodes explored in the forward BFS)
+    backward_explored = [] # creates variable backward_explored as an empty array (used to keep track of all nodes explored in the backward BFS)
+    while True: #while loop to run until a path is found
+        path_foward = queue_foward.pop(0) # removes the path at the top of the queue to be explored
+        node_foward = path_foward[-1] # gets the last node in the path which is where we will check for new paths      
+        if node_foward in backward_explored: # condition to check if the backward BFS has explored the foward node
+            for i in range(len(backward_explored)): # used to check each possible path in the backward search for the node_foward
+                if node_foward in queue_backward[-i]: #checks if foward_node is in each path in queue_backward (starts from the back as this is where it is most likely to be and helps reduce time of the program)
+                    reverse_list = (queue_backward[-i])[::-1] # reverse the list that contains foward_node
+                    path = path_foward + reverse_list # concatinates the foward and backward path together
+                    for x in path: #removes any duplicate nodes from the path
                         if path.count(x) > 1:
                             path.remove(x)
-                            print("repeated node : " + str(x))
-                    print("path : " + str(path))
-                    print("path length : " + str(len(path)))
-                    print("nodes visited : " + str(nodes))
-                    maze_visited(foward_explored+backward_explored,maze)
-                    maze_solution(path,maze)
-                    return                    
-        if node_foward not in foward_explored:
-        #calculate all possible directions from current node
+                    print("path : " + str(path)) #outputs the path coordinates to the terminal
+                    print("path length : " + str(len(path))) #outputs the length of the path to the terminal
+                    print("nodes visited : " + str(len(foward_explored+backward_explored))) #outputs the number of explored nodes into the terminal
+                    maze_visited(foward_explored+backward_explored,maze) #runs a function that outputs the explored nodes onto a text file of the map
+                    maze_solution(path,maze) #runs a function that outputs the path onto the text file of the map
+                    return # ends the search                   
+        if node_foward not in foward_explored: #checks to see if the node_foward has already been explored
+            #calculate all possible directions from current node and stores in possible_node
             possible_node = []
             if maze[node_foward[1]][node_foward[0]-1] == "-":
                 possible_node.append((node_foward[0]-1,node_foward[1]))
@@ -56,12 +46,13 @@ def breadth_first_search_loop(maze,queue_foward,queue_backward):
                 possible_node.append((node_foward[0],node_foward[1]-1))    
             if maze[node_foward[1]+1][node_foward[0]] == "-":
                 possible_node.append((node_foward[0],node_foward[1]+1))
-            for x in possible_node:
-                nodes += 1
-                new_path = list(path_foward)
-                new_path.append(x)
-                queue_foward.append(new_path)
-            foward_explored.append(node_foward)
+            for x in possible_node: #for each possible node we create a new path and add to the back of the queue
+                new_path = list(path_foward) #creates a new path that consists of path_foward and possible node added to the end
+                new_path.append(x) #adds possible_node to the new_path
+                queue_foward.append(new_path) #adds new_path to the queue
+            foward_explored.append(node_foward) #adds node_forward to foward_explored
+
+        #this is exactly the same functionality just going backward from the goal
         path_backward = queue_backward.pop(0)
         node_backward = path_backward[-1]
         if node_backward not in backward_explored:
@@ -76,12 +67,12 @@ def breadth_first_search_loop(maze,queue_foward,queue_backward):
                 if maze[node_backward[1]+1][node_backward[0]] == "-":
                     possible_node.append((node_backward[0],node_backward[1]+1))
             for x in possible_node:
-                nodes += 1
                 new_path = list(path_backward)
                 new_path.append(x)
                 queue_backward.append(new_path)
             backward_explored.append(node_backward)
 
+#this is an extra function that outputs the path onto the maze-solution.txt text file
 def maze_visited(visited,maze):
     for i in range(len(visited)):
         maze[visited[i][1]][visited[i][0]] = "x"
@@ -91,6 +82,7 @@ def maze_visited(visited,maze):
     f = open("Visited-Nodes.txt", "w")
     f.write('\n'.join(join_visited))
 
+#this is an extra function that outputs all the nodes visited onto the Visted-Nodes.txt text file
 def maze_solution(path,maze):
     for i in range(len(path)):
         maze[path[i][1]][path[i][0]] = "x"
@@ -100,4 +92,5 @@ def maze_solution(path,maze):
     f = open("maze-solution.txt", "w")
     f.write('\n'.join(join_visited))
 
-breadth_first_search("maze-Medium")
+#add the maze file name here to carry out the search
+breadth_first_search("maze-Large")
--- DFS.py
+++ DFS.py
@@ -1,30 +1,29 @@
 import time
 
 def depth_first_search(file_name):
-    st =  time.time()
-    f = open(file_name+".txt", "r")
-    data = f.readlines()
+    st =  time.time() #stores the time when the search starts
+    f = open(file_name+".txt", "r") #opens maze file 
+    data = f.readlines() #splits the maze file lines into individual elements
     maze = []
-    for x in data:
+    for x in data: #splits each line into individual elements in the maze (# , -)
         if x.split != []:
             maze.append(x.split())
-    start_position = (maze[0].index('-'),0)
-    goal = (maze[len(maze)-1].index('-'),len(maze)-1)
-    depth_first_search_loop(maze,start_position,goal,[start_position],[start_position])
-    et = time.time()
-    print(file_name + " took " + str(et-st) + " seconds to execute" )
+    start_position = (maze[0].index('-'),0) # finds the start position of the maze
+    goal = (maze[len(maze)-1].index('-'),len(maze)-1) # finds the end position of the maze
+    depth_first_search_loop(maze,start_position,goal,[start_position],[start_position])  # calls the loop for the dfs giving it the maze , start position , goal, a path with start position already in and a list of nodes which have been visited
+    et = time.time() #stores the end time of the search
+    print(file_name + " took " + str(et-st) + " seconds to execute" ) #calculates total time for search to complete and outputs
 
 def depth_first_search_loop(maze,current_node,goal,path,visited):
-    nodes = 1
     while True:
-        if current_node == goal:
-            maze_solution(path,maze) # runs function that outputs a file that shows path on the maze
-            print(path)
-            print("steps in path : " + str(len(path)))
-            print("nodes visited : " + str(nodes))
-            break
+        if current_node == goal:#checks to see if the current node is the goal
+            maze_solution(path,maze) # runs function that outputs a file that shows the path on the maze
+            print(path) # outputs path coordiantes into the terminal
+            print("steps in path : " + str(len(path))) # outputs length of path into terminal
+            print("nodes visited : " + str(len(visited))) # outputs number of nodes visited into the terminal
+            break # ends search
 
-        #calculate all possible directions from current node
+        #calculate all possible directions from current node and stores in possible_node
         possible_node = []
         if maze[current_node[1]][current_node[0]-1] == "-":
             possible_node.append((current_node[0]-1,current_node[1]))
@@ -35,23 +34,21 @@ def depth_first_search_loop(maze,current_node,goal,path,visited):
         if maze[current_node[1]+1][current_node[0]] == "-":
             possible_node.append((current_node[0],current_node[1]+1))
 
-        #check if possible directions against visted
-        new_node_found =  False
-        for x in possible_node:
-            if x not in visited:
-                current_node = x
-                path.append(x)
-                visited.append(x)
-                nodes +=1
-                new_node_found = True
-                break
         
-        if new_node_found != True:
-            if (path == []):
-                maze_visited(visited,maze)
-            path.pop()
-            current_node = path[len(path)-1]
+        new_node_found =  False
+        for x in possible_node: # checks against each node in possible_node
+            if x not in visited: # checks if node hasn't been visited
+                current_node = x # sets node to current node
+                path.append(x) # adds node to the path
+                visited.append(x) # adds node to visited
+                new_node_found = True # used to check if a unvisited node has been found
+                break # ends loop after a new node has been found
+        
+        if new_node_found != True: # this section of code runs if a new node hasn't been found from current node 
+            path.pop() # removes most recent node from the path
+            current_node = path[len(path)-1] #sets current node as last node in path so the search can backtrack
 
+#this is an extra function that outputs the path onto the maze-solution.txt text file
 def maze_visited(visited,maze):
     for i in range(len(visited)):
         maze[visited[i][1]][visited[i][0]] = "x"
@@ -61,6 +58,7 @@ def maze_visited(visited,maze):
     f = open("Visited-Nodes.txt", "w")
     f.write('\n'.join(join_visited))
 
+#this is an extra function that outputs all the nodes visited onto the Visted-Nodes.txt text file
 def maze_solution(path,maze):
     for i in range(len(path)):
         maze[path[i][1]][path[i][0]] = "x"
@@ -70,4 +68,5 @@ def maze_solution(path,maze):
     f = open("maze-solution.txt", "w")
     f.write('\n'.join(join_visited))
 
-depth_first_search("maze-Medium")
+#add the maze file name here to carry out the search
+depth_first_search("maze-Small")
